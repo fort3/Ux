02dd35a7f4e355ccbb0725ca2c5300c0127fd160
Feli <felromero9@gmail.com>
2020-07-01 12:53:23 +0200

    add carusel pictures

--- js/main.js
+++ js/main.js
@@ -69,15 +69,40 @@ $('#contactNav').click(() => {
 // Vue modal forms
 ///////////////////////////////
 
- // register modal component
- Vue.component("modal", {
+// register modal component
+Vue.component("modal", {
   template: "#modal-template"
 });
 
 // start app
-new Vue({
+
+
+const app = new Vue({
   el: "#app",
   data: {
-    showModal: false
+    showModal: false,
+    message: 'test vuejs',
+    selected: '',
+    errors: [],
+    name: null,
+    email: null,
+    guests: null
+  },
+  methods: {
+    checkForm: function (e) {
+      this.errors = [];
+      if (!this.name) this.errors.push("Name required!");
+      if (!this.email) {
+        this.errors.push("Email required!");
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push("Valid email required!");
+      }
+      if (!this.errors.length) return true;
+      e.preventDefault();
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
   }
-});
+})
